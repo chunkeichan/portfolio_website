51911d57e9200de6c0676880bf58a46a02e47272
lvey1901 <chunkeichan1901@gmail.com>
2022-10-26 00:07:52 +0800

    test on react-device-detect - isMobile

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import Cover from './webpages/Cover';
 import Contact from './webpages/Contact';
 import Project from './webpages/Project';
 import Webpages from './webpages';
+import { MobileView, isMobile } from 'react-device-detect';
 
 function App() {
   const [windowDimension, setWindowDimension] = useState(window.innerWidth)
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimension(window.innerWidth)
-    }
-    // Check for the relationship between addEventListener and removeEventListener
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [])
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowDimension(window.innerWidth)
+  //   }
+  //   // Check for the relationship between addEventListener and removeEventListener
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, [])
 
-  const isMobile = windowDimension <= 700 //640 for production
+  // const isMobile = windowDimension <= 700 //640 for production
 
   return (
     <div>
