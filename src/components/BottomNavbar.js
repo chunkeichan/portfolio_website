a1fe85e9f120fe5f40d1f774b67a42985f5f281f
lvey1901 <chunkeichan1901@gmail.com>
2022-10-28 22:50:48 +0800

    try onClick div

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -17,9 +17,9 @@ function BottomNavbar() {
           <img className="mobile--nav--buttonpic" src="./images/profile_icon.svg"></img>
           <button className="mobile--nav--button" id="profile" onClick={handleClick}>Profile</button>
         </div>
-        <div className="nav--buttonbox">
+        <div className="nav--buttonbox" onClick={handleClick}>
           <img className="mobile--nav--buttonpic" src="./images/project_icon.svg"></img>
-          <button className="mobile--nav--button" id="project" onClick={handleClick}>Projects</button>
+          <button className="mobile--nav--button" id="project">Projects</button>
         </div>
         <div className="nav--buttonbox">
           <img className="mobile--nav--buttonpic" src="./images/contact_icon.svg"></img>
