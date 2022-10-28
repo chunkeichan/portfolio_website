d5814e1710233d2be50b9a8dde91fc817fac9525
lvey1901 <chunkeichan1901@gmail.com>
2022-10-29 00:49:58 +0800

    test bottomnav buttons

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -13,17 +13,17 @@ function BottomNavbar() {
   return (
     <div className="mobile--bottomnav">
       <div className="mobile--nav--select">
-        <div className="mobile--nav--buttonbox">
+        <div className="mobile--nav--buttonbox" id="profile" onClick={handleClick}>
           <img className="mobile--nav--buttonpic" src="./images/profile_icon.svg"></img>
-          <button className="mobile--nav--button" id="profile" onClick={handleClick}>Profile</button>
+          <button className="mobile--nav--button">Profile</button>
         </div>
-        <div className="mobile--nav--buttonbox" onClick={handleClick} id="project">
+        <div className="mobile--nav--buttonbox" id="project" onClick={handleClick}>
           <img className="mobile--nav--buttonpic" src="./images/project_icon.svg"></img>
           <button className="mobile--nav--button">Projects</button>
         </div>
-        <div className="mobile--nav--buttonbox">
+        <div className="mobile--nav--buttonbox" id="contact" onClick={handleClick}>
           <img className="mobile--nav--buttonpic" src="./images/contact_icon.svg"></img>
-          <button className="mobile--nav--button" id="contact" onClick={handleClick}>Contact</button>
+          <button className="mobile--nav--button">Contact</button>
         </div>
       </div>
     </div>
