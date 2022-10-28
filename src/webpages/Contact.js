228846749a7ab7dad2d20babf7a4bfae17d89457
lvey1901 <chunkeichan1901@gmail.com>
2022-10-29 01:25:06 +0800

    adjust bottomnav positioning and add margin to web view

--- src/webpages/Contact.js
+++ src/webpages/Contact.js
@@ -10,24 +10,24 @@ function Contact(props) {
   
   return (
     <div className={checkIsMobile("contact")} data-aos="fade-up" data-aos-anchor-placement="top-center">
-    {/* <img className="contact--pic" src={profile_pic}></img> */}
-    <img className={checkIsMobile("contact--pic")} src="https://chunkeichan.github.io/portfolio_website/images/DSCF9931_adjusted_9.webp"></img>
-    
-    <div className={checkIsMobile("contact--methodbox")}>
-      <div className={checkIsMobile("contact--method")}>
-        {/* <img src="../images/logos_linkedin-icon.svg"></img> */}
-        <img src="https://chunkeichan.github.io/portfolio_website/images/logos_linkedin-icon.svg"></img>
-        <p>chunkeichan1901@gmail.com</p>
-      </div>
-      <br/>
-      <br/>
-      <div className={checkIsMobile("contact--method")}>
-        {/* <img src="../images/line-md_email-twotone.svg"></img> */}
-        <img src="https://chunkeichan.github.io/portfolio_website/images/line-md_email-twotone.svg"></img>
-        <p>chunkeichan1901@gmail.com</p>
+      {/* <img className="contact--pic" src={profile_pic}></img> */}
+      <img className={checkIsMobile("contact--pic")} src="https://chunkeichan.github.io/portfolio_website/images/DSCF9931_adjusted_9.webp"></img>
+      
+      <div className={checkIsMobile("contact--methodbox")}>
+        <div className={checkIsMobile("contact--method")}>
+          {/* <img src="../images/logos_linkedin-icon.svg"></img> */}
+          <img src="https://chunkeichan.github.io/portfolio_website/images/logos_linkedin-icon.svg"></img>
+          <p>chunkeichan1901@gmail.com</p>
+        </div>
+        <br/>
+        <br/>
+        <div className={checkIsMobile("contact--method")}>
+          {/* <img src="../images/line-md_email-twotone.svg"></img> */}
+          <img src="https://chunkeichan.github.io/portfolio_website/images/line-md_email-twotone.svg"></img>
+          <p>chunkeichan1901@gmail.com</p>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
