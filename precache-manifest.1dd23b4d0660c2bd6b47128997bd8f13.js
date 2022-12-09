22612e685839e8ded21add3b11fc1c7f6335b85e
lvey1901 <chunkeichan1901@gmail.com>
2022-12-10 02:20:47 +0800

    Updates

---
+++ precache-manifest.1dd23b4d0660c2bd6b47128997bd8f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c49d43205b9f13c06a",
+    "url": "/static/css/main.a6da2bf9.chunk.css"
+  },
+  {
+    "revision": "47c49d43205b9f13c06a",
+    "url": "/static/js/main.47c49d43.chunk.js"
+  },
+  {
+    "revision": "af0aac77d8aecc35095c",
+    "url": "/static/js/1.af0aac77.chunk.js"
+  },
+  {
+    "revision": "6ba7a3fff527d95b2f43",
+    "url": "/static/css/2.e5c119b0.chunk.css"
+  },
+  {
+    "revision": "6ba7a3fff527d95b2f43",
+    "url": "/static/js/2.6ba7a3ff.chunk.js"
+  },
+  {
+    "revision": "dbdaedf75edbbc9f38c7",
+    "url": "/static/js/runtime~main.dbdaedf7.js"
+  },
+  {
+    "revision": "0024ddb7629d3f174f57727ade9e0413",
+    "url": "/index.html"
+  }
+];
