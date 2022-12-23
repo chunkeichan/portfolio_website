0565630dff1a7e56e08458f5824e83eace9b4d17
lvey1901 <chunkeichan1901@gmail.com>
2022-12-23 23:38:08 +0800

    add hyperlinks at Contact

--- src/webpages/Contact.js
+++ src/webpages/Contact.js
@@ -7,6 +7,8 @@ function Contact(props) {
   }
 
   const profile_pic = "../images/DSCF9931_adjusted_9.jpg"
+  const url_linkedin = "https://hk.linkedin.com/in/jacky-ckchan"
+  const url_email = "chunkeichan1901@gmail.com"
   
   return (
     <div className={checkIsMobile("contact")} data-aos="fade-up" data-aos-anchor-placement="top-center">
@@ -14,18 +16,22 @@ function Contact(props) {
       <img className={checkIsMobile("contact--pic")} src="https://chunkeichan.github.io/portfolio_website/images/DSCF9931_adjusted_9.webp"></img>
       
       <div className={checkIsMobile("contact--methodbox")}>
-        <div className={checkIsMobile("contact--method")}>
-          {/* <img src="../images/logos_linkedin-icon.svg"></img> */}
-          <img src="https://chunkeichan.github.io/portfolio_website/images/logos_linkedin-icon.svg"></img>
-          <p>chunkeichan1901@gmail.com</p>
-        </div>
+        <a href={url_linkedin} style={{textDecoration: "none", color: "black"}}>
+          <div className={checkIsMobile("contact--method")}>
+            {/* <img src="../images/logos_linkedin-icon.svg"></img> */}
+            <img src="https://chunkeichan.github.io/portfolio_website/images/logos_linkedin-icon.svg"></img>
+            <p href={url_linkedin}>chunkeichan1901@gmail.com</p>
+          </div>
+        </a>
         <br/>
         <br/>
-        <div className={checkIsMobile("contact--method")}>
-          {/* <img src="../images/line-md_email-twotone.svg"></img> */}
-          <img src="https://chunkeichan.github.io/portfolio_website/images/line-md_email-twotone.svg"></img>
-          <p>chunkeichan1901@gmail.com</p>
-        </div>
+        <a href={`mailto:${url_email}`} style={{textDecoration: "none", color: "black"}}>
+          <div className={checkIsMobile("contact--method")}>
+            {/* <img src="../images/line-md_email-twotone.svg"></img> */}
+            <img src="https://chunkeichan.github.io/portfolio_website/images/line-md_email-twotone.svg"></img>
+            <p >chunkeichan1901@gmail.com</p>
+          </div>
+        </a>
       </div>
     </div>
   )
